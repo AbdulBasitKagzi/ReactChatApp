119a9292730c1fc074ea44392dc1ca6539a3a852
Lcom <imabk10@gmail.com>
2022-11-12 13:23:00 +0530

    added call leave functionality

--- abdulchatapp/src/Button.js
+++ abdulchatapp/src/Button.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import { useHMSActions, useHMSStore, selectPeers } from "@100mslive/react-sdk";
+import {
+  useHMSActions,
+  useHMSStore,
+  selectPeers,
+  useHMSNotifications,
+  HMSNotificationTypes,
+} from "@100mslive/react-sdk";
 function Button() {
   const buttonpeers = useHMSStore(selectPeers);
-  console.log("buttonPeer", buttonpeers);
   const hmsActions = useHMSActions();
+  const notification = useHMSNotifications();
+
+  useEffect(() => {
+    if (!notification) {
+      return;
+    } else if (
+      notification?.type === HMSNotificationTypes.PEER_LEFT &&
+      buttonpeers.length === 1
+    ) {
+      console.log("Everyone left");
+      hmsActions.leave();
+    }
+  }, [notification]);
 
   // tried something but didnot work
-  // const handleLeave = () => {
-  //   if(buttonpeers.length!==0 || buttonpeers.length === 1){
-  //     hmsActions.leave();
-  //   }
-    
-  // };
+  const handleLeave = () => {
+    hmsActions.leave();
+
+    console.log("poppeers", buttonpeers);
+  };
 
   return (
     <div>
-      <button id="leave-btn" className="btn-danger" onClick={()=>hmsActions.leave()}>Leave</button>
+      <button id="leave-btn" className="btn-danger" onClick={handleLeave}>
+        Leave
+      </button>
     </div>
   );
 }
